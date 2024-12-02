07776016c4db17c0dcb686c25a1c545eece81160
Wen Jun Jie <132931864+WenJJ2000@users.noreply.github.com>
2024-12-02 19:15:47 +0800

    Delete src/apiConfig.js
    removed api_key

--- src/apiConfig.js
+++
@@ -1,4 +0,0 @@
-export const API_KEY = "cec86edc91msh4a3b46408546164p1b6501jsnea9460095cc8";
-export const API_HOST = "google-translate1.p.rapidapi.com";
-export const BASE_URL =
-  "https://google-translate1.p.rapidapi.com/language/translate/v2";
